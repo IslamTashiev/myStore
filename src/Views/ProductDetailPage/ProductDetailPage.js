00494a952fcd4141235b2230b1516067d3c28435
IslamTashiev <ueaten04@gmail.com>
2021-06-30 02:01:49 +0600

    initial files

---
+++ src/Views/ProductDetailPage/ProductDetailPage.js
@@ -0,0 +1,64 @@
+import { IconButton, Typography } from '@material-ui/core';
+import React, { useContext, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router';
+import Loader from '../../components/Loader';
+import { storeContext } from '../../contexts/StoreContext';
+import MainLayout from '../../Layouts/MainLayout';
+import ProductSlider from './components/ProductSlider';
+import classes from './productDetail.module.css';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import { useConfirm } from 'material-ui-confirm';
+import { notifySuccess } from '../../helpers/notifiers';
+
+export default function ProductDetailPage() {
+  const { fetchProductDetail, productDetail, deleteProduct } =
+    useContext(storeContext);
+
+  const { id } = useParams();
+  const confirm = useConfirm();
+  const history = useHistory();
+
+  useEffect(() => {
+    fetchProductDetail(id);
+  }, [id]);
+
+  const handleProductDelete = () => {
+    confirm({
+      description: 'Удалить данный товар?',
+    }).then(() => {
+      deleteProduct(id).then(() => {
+        notifySuccess('Товар был успешно удален!');
+        history.push('/');
+      });
+    });
+  };
+
+  return (
+    <MainLayout>
+      {productDetail ? (
+        <div className={classes.container}>
+          <ProductSlider images={productDetail.images} />
+
+          <div>
+            <IconButton onClick={handleProductDelete}>
+              <DeleteIcon />
+            </IconButton>
+
+            <IconButton onClick={() => history.push(`/products/${id}/update`)}>
+              <EditIcon />
+            </IconButton>
+          </div>
+
+          <Typography variant="h5">Модель - {productDetail.title}</Typography>
+          <Typography variant="h5">Год - {productDetail.year}</Typography>
+          <Typography variant="h5">Объем - {productDetail.engine}</Typography>
+          <Typography variant="h5">Цена - {productDetail.price} $</Typography>
+          <Typography variant="body1">{productDetail.description}</Typography>
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </MainLayout>
+  );
+}
